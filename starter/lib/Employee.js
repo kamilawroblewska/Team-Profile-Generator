b60b845a82775379716024f386fd5b33b7a51039
kamilawroblewska <kamila.wroblewska15@gmail.com>
2024-02-26 12:51:31 +0000

    Added Employee class with methods: getName, getId, getEmail, getRole; test passed

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+      // Save a reference for `this` in `this` as `this` will change inside of inquirer
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    // Methods
+    getName() {
+        return this.name;
+    }
+    getId() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getRole() {
+        return 'Employee';
+    }
+}
+
+module.exports = Employee;
